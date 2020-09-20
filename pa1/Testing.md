ab408a5657dee52e56a2affe1694ac227aed645d
Justin Guerrero <justin.guerrero@ecat1.montana.edu>
2020-09-20 11:58:17 -0600

    latest commit with all the things

---
+++ pa1/Testing.md
@@ -0,0 +1,18 @@
+#Testing
+
+##Testing memory locations with gdb
+In this small write up I will talk about how I tested my program with gdb and found the locations of my files in memory.
+First thing that I needed to do was run gdb.
+
+``` gbd -q album ```
+
+Once the gdb was started I placed a breakpoint at each function. In the video I used the resizePhoto function. 
+
+``` b resizePhoto ```
+
+Then I wanted to pass it some arguments so I could find out where the programs memory jumped to and from.
+
+`` r photos/*.jpg ```
+
+This passed in all the photos (the whopping 2 photos) from my photo album into the gdb to be evaluated.
+
--- pa1/album.c
+++ pa1/album.c
@@ -8,7 +8,6 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-
 int resizePhoto(const char * program, char ** arg_list) { 
     //use the command line convert program from imagemagick
 	
@@ -46,26 +45,35 @@ int displayPhoto(const char *program3, char ** arg_list3){
   } 
 }
 
-// void askUserRotate(){
-    // //use the convert program here again with rotate function
-// }
 
-// char askForCaption(){
-    // char captionInput[80];
-    // //asks user for caption
-    // printf("What would you like to caption this photo: ");
-    // scanf("%[^\n]%*c", captionInput);
-    // printf("the photo now has the caption: %s \n", captionInput);
-    // return(0);
-// }
+char askUserRotate(const char *program4, char ** arg_list4, char *r){
+    //use the convert program here again with rotate function
+	printf("How much do you want to rotate this photo: ");
+    scanf("%[^\n]%*c", r);
+    printf("the photo now has been rotated: %s \n", r);
+		  pid_t child_pid = fork();
+  if (child_pid != 0)
+    return child_pid;      /* This is the parent process.  */
+  else {
+    execvp(program4, arg_list4);     /* Now execute PROGRAM */
+	
+	fprintf (stderr, "An error occurred in execvp\n");
+    abort ();
+  } 
+    return *r;
+}
 
-// void orientatePhoto(){
+char askForCaption(char c[]){
+    //asks user for caption
+    printf("What would you like to caption this photo: ");
+    scanf("%[^\n]%*c", c);
+    printf("the photo now has the caption: %s \n", c);
+    return *c;
+}
 
-    // // use convert program here
-// }
 
-int makeHTML(){
-	//do html stuff here
+int makeHTML(char* c){
+	// fprinf to print html code to the "index.html" file
 	FILE *fptr = fopen("index.html","w");
 	if(fptr==NULL){
 		printf("Couldn't open file");
@@ -73,22 +81,49 @@ int makeHTML(){
 	}
 	else{
 		fprintf(fptr, "Content-Type: text/html\n\n");
-		fprintf(fptr, "<html>");
-		fprintf(fptr, "<head><title>Photo Album</title></head>");
-		fprintf(fptr, "<body>here is a body</body>");
+		fprintf(fptr, "<html>\n\n");
+		fprintf(fptr, "<head><title>Photo Album</title></head>\n\n");
+		fprintf(fptr, "<body>here is your photo album, enjoy</body>\n\n");
+		fprintf(fptr, "<div class='row'>\n\n");
+		fprintf(fptr, "<img src='photos/newVedraPhotoThumb.png'>\n\n");
+		fprintf(fptr, "</div class='row'>\n\n");
+		fprintf(fptr, "<div class='row'>\n\n");
+		fprintf(fptr, "<img src='photos/>\n\n");
+		fprintf(fptr, "</div class='row'>\n\n");
 	}
 	return 0;
 		
 }
 
-int main() { 
-  makeHTML();
-  char *arg_list3[] = {"display", "photos/newVedraPhotoThumb.png",NULL};  
-  char *arg_list2[] = {"convert", "photos/Es_Vedra_Sunset.jpg", "-resize", "25%", "photos/newVedraPhotoThumb.png",NULL};  
-  char *arg_list[] = {"convert", "photos/Es_Vedra_Sunset.jpg", "-resize", "75%", "photos/newVedraPhoto.png",NULL};
+
+int main(int argc, char *argv[]) {
+  // create arrays for the user input
+  char c[90];	 // caption
+  char rr[10];	 // rotate
+  
+  //input functions from user are placed here to gather values for arguments
+  askForCaption(c);
+  char *newC = strcat(c, "thumb.png");
+  char str[100] = "photos/";
+  char *newestC = strcat(str, newC);
+
+  
+  // create arg strings for function inputs and calls
+  char *arg_list[] =  {"convert", argv[1], "-resize", "75%", argv[1], NULL};		// resize to 75%	
+  char *arg_list2[] = {"convert", argv[1], "-resize", "25%", newestC, NULL};	// create thumbnail
+  char *arg_list4[] = {"convert", argv[1], "-rotate", rr, "rotated.png", NULL};		// rotate image
+  char *arg_list3[] = {"display", newestC ,NULL}; 												// display image	
+  
+
+  // call functions and hope they work
+  
+  askUserRotate("convert", arg_list4, rr); // concat string for thumb
   resizePhoto("convert", arg_list); 
   thumbNail("convert", arg_list2);
+  sleep(5);
   displayPhoto("display", arg_list3);
+  makeHTML(c);
   printf ("Main program exiting...\n");
+  printf("hi");
   return 0; 
 }
